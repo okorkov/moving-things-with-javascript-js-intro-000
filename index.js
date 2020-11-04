093e0006aa8d0cad6c83743ada35e383cb4b2912
Aliaksandr Okarkau <alex.okorkov@gmail.com>
2020-11-04 00:21:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function moveDodgerDown() {
   var downNumbers = dodger.style.bottom.replace('px', '')
   var down = parseInt(upNumbers, 10)
  
-  if (down >= 0) {
+  if (down > 0) {
     dodger.style.bottom = `${down - 15}px`
   }
 }
